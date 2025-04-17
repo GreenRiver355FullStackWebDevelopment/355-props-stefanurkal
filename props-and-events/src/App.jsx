bb2d708b6d6ff7daea5baab009e29a26c29e00fd
Stefanurkal <stephenfranada@gmail.com>
2025-04-17 10:34:50 -0700

    completed lab, gave you the heads up it would be late thank you

--- props-and-events/src/App.jsx
+++ props-and-events/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import RestaurantsContainer from "./components/RestaurantsContainer.jsx"
 
 function App() {
   return (
     <div className="App">
-
+      <RestaurantsContainer/>
     </div>
   );
 }
--- props-and-events/src/components/RestaurantsContainer.jsx
+++ props-and-events/src/components/RestaurantsContainer.jsx
@@ -1,7 +1,10 @@
+import { restaurants } from "../data/data.js";
+import Restaurant from "./Restaurant";
+
 function RestaurantsContainer() {
   return (
     <div className="restaurantContainer">
-
+      {restaurants.map(restaurant => <Restaurant restaurant={restaurant} />)}
     </div>
   );
 }
